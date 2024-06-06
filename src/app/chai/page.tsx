955c810ef776e4b4df2ca54214494a8c3b1f2fa3
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-06-06 15:28:27 +0530

    next-js setup

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,5 @@
+export default function chaiPage(){
+    return (
+        <h1 className="text-center text-3xl mt-4">Chai Page</h1>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+   <h1 className="text-center text-2xl">
+    chai or code
+   </h1> 
+  );
+}
