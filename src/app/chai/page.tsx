18f85a7800dac8c0664e29a2e0314397f9f3b602
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-06-07 08:33:44 +0530

    explore acertinity library

--- src/app/chai/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function chaiPage(){
-    return (
-        <h1 className="text-center text-3xl mt-4">Chai Page</h1>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-   <h1 className="text-center text-2xl">
-    chai or code
-   </h1> 
+   <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <h1 className="text-center text-2xl">Chai or code</h1>
+   </main>
   );
 }
