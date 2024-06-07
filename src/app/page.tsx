47f255888f56af276426c050d496e97983a1b154
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-06-07 12:41:04 +0530

    know about typescript and aceternity library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
    <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-    <h1 className="text-center text-2xl">Chai or code</h1>
+    <HeroSection/>
+    <FeaturedCourses/>
    </main>
   );
 }
