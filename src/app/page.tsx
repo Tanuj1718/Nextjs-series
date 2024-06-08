389478665e51e53df34bf9ee3eb619e8563ea1de
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-06-09 00:01:54 +0530

    animated cards and wavy background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import AnimatedTooltipPreview from "@/components/AnimatedTooltipPreview";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -10,6 +12,8 @@ export default function Home() {
     <FeaturedCourses/>
     <WhyChooseUs/>
     <MusicSchoolTestimonials/>
+    <UpcomingWebinars/>
+    <AnimatedTooltipPreview/>
    </main>
   );
 }
